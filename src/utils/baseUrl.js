9900c248352b65d0fcc4846cac925b12716457eb
krishna-ag-github <117165592+krishna-ag-github@users.noreply.github.com>
2024-02-26 17:42:08 +0530

    url updated

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,1 +1,2 @@
-export const base_url = "http://localhost:5000/api/";
+// export const base_url = "http://localhost:5000/api/";
+export const base_url = "https://agk-mern-ecommerce-api.onrender.com";
